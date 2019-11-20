f04eb792fae9f0a283667554e7994fb1fc9b795a
Karolina Pieszczek <34057260+pyszczekk@users.noreply.github.com>
2019-11-20 13:29:36 +0100

    Add files via upload

--- lab7/lab7.m
+++ lab7/lab7.m
@@ -12,7 +12,7 @@ subplot(1,3,2);
 imshow(convolution(I_noisefree, [3, 3]), []);
 title('convolution');
 subplot(1,3,3)
-imshow(bilateral(I_noisefree, [3, 3],3), [])
+imshow(bilateral(I_noisefree, [3, 3],1), [])
 title('bilateral')
 
 figure(2);
@@ -23,7 +23,7 @@ subplot(1,3,2);
 imshow(convolution(I_noisy1, [3, 3]), []);
 title('convolution');
 subplot(1,3,3)
-imshow(bilateral(I_noisy1, [3, 3],0.5), [])
+imshow(bilateral(I_noisy1, [3, 3],7), [])
 title('bilateral')
 
 figure(3);
@@ -34,7 +34,7 @@ subplot(1,3,2);
 imshow(convolution(I_noisy2, [3, 3]), []);
 title('convolution');
 subplot(1,3,3)
-imshow(bilateral(I_noisy2, [3, 3],0.05), [])
+imshow(bilateral(I_noisy2, [3, 3],100), [])
 title('bilateral')
 
 
@@ -46,7 +46,7 @@ subplot(1,3,2);
 imshow(convolution(I_noisy3, [3, 3]), []);
 title('convolution');
 subplot(1,3,3)
-imshow(bilateral(I_noisy3, [3, 3],3), [])
+imshow(bilateral(I_noisy3, [3, 3],5), [])
 title('bilateral')
 
 figure(5);
@@ -57,5 +57,5 @@ subplot(1,3,2);
 imshow(convolution(I_noisy4, [3, 3]), []);
 title('convolution');
 subplot(1,3,3)
-imshow(bilateral(I_noisy4, [3, 3],3), [])
+imshow(bilateral(I_noisy4, [3, 3],15), [])
 title('bilateral')
