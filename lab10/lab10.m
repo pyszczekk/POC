c12bbb06ecd548d016f5df57a5e6d8220d973e5f
Karolina Pieszczek <karolina.pieszczekk@gmail.com>
2019-12-08 15:25:25 +0100

    lab10

---
+++ lab10/lab10.m
@@ -0,0 +1,59 @@
+close all;
+clearvars;
+clc;
+
+img = imread('knee.png');
+
+figure(1);
+imshow(img);
+title('oryginal');
+%[x, y] = ginput(1);
+%x = floor(x);
+%y = floor(y);
+x=340;
+y=250;
+
+img = double(img);
+
+[X, Y] = size(img);
+
+visited = zeros(X, Y);
+segmented = zeros(X, Y);
+
+stack = zeros(10000, 2);
+iStack = 1;
+
+visited(x, y) = 1;
+segmented(x, y) = 1;
+
+stack(iStack, 1:2) = [x, y];
+
+tollerance = 40;
+nS = 0;
+mV = 0;
+while iStack > 0
+    px = stack(iStack, 1);
+    py = stack(iStack, 2);
+    iStack = iStack-1;
+    nS =nS + 1;
+    mV = (mV * (nS - 1) + img(px, py)) / nS;
+    
+    if px > 1 && px < X && py > 1 && py < Y
+        for ix = px-1:px+1
+            for iy = py-1:py+1
+                if ix~=px && iy~=py && visited(ix,iy)==0 && ((abs(img(ix,iy) - mV)) < tollerance)
+                    iStack=iStack+1;
+                    stack(iStack, :) = [ix, iy];
+                    segmented(ix,iy) = img(ix,iy);
+                elseif ix==px && iy==py
+                    segmented(ix,iy) = img(ix,iy);
+                end
+                visited(ix,iy)=1;
+            end
+        end    
+    end
+end
+
+figure(2);
+imshow(segmented,[]);
+title('segmentacja');
---
+++ lab10/lab10b.m
@@ -0,0 +1,92 @@
+close all;
+clearvars;
+clc;
+
+img = imread('umbrealla.png');
+
+figure(1);
+subplot(2,2,1)
+imshow(img);
+title('oryginal');
+
+
+img = rgb2hsv(img);
+imgH = double(img(:,:,1));
+
+subplot(2,2,2);
+imshow(imgH,[]);
+title('H');
+
+global sLimit vLimit MRes segRes index; % do split
+sLimit = 4;
+vLimit = 0.05;
+index = -1;
+
+[y, x] = size(imgH);
+
+segRes = zeros(y, x);
+MRes = zeros(y, x);
+
+split(imgH, 1, 1, x, y);
+
+colorThreshold = 5/255;
+minAreaSize = 27;
+
+i = 0;
+while i <= index
+   IB = segRes == i;
+   
+   if any(IB(:))
+       [yF, xF] = find(IB, 1, 'first');
+       
+       square = strel('square', 3);
+       neighbors = imdilate(IB, square);
+       diff = imabsdiff(neighbors, IB);
+       pointMult = diff .* segRes;
+       nonZeros = nonzeros(pointMult);
+       uniqued = unique(nonZeros);
+       
+       isJoined = 0;
+       for neighbor = 1:numel(uniqued)
+           IBS = segRes == uniqued(neighbor);
+           
+           [yFS, xFS] = find(IBS, 1, 'first');
+           
+           colorDiff = abs(MRes(yF,xF) - MRes(yFS, xFS));
+           if colorDiff < colorThreshold
+               segRes(IBS) = i;
+               isJoined = 1;
+           end
+       end
+       if isJoined == 0
+          i = i + 1;
+       end
+   else
+       i = i + 1;
+   end
+end
+
+subplot(2,2,3)
+imshow(segRes,[]);
+title('segRes');
+
+U = unique(segRes);
+
+for idx = 1:numel(U)
+    C = segRes == U(idx);
+    if sum(C) < minAreaSize
+       segRes(C) = 0; 
+    end
+end
+
+U = unique(segRes);
+
+for idx = 1:numel(U)
+    C = segRes == U(idx);
+    segRes(C) = idx;
+end
+finalImage = label2rgb(segRes);
+
+subplot(2,2,4);
+imshow(finalImage);
+title('final')
